2919e678160b8d9f64537d1abc32f918a776c510
easily-mistaken <choubeyprajjwal@gmail.com>
2024-11-27 23:57:38 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(port, () => console.log("Server is listening on port: ", port));
